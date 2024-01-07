dc3d21b871bf18096e3d415918bcc964892051c1
Isak Bego <isakbego@gmail.com>
2024-01-07 15:59:23 +0100

    Manipulated and stored the data in the desired format

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./style.css";
+import { getDay } from "date-fns";
 
 //you can get it, it's for free
 const API_KEY = "0cf899136a814fb3a74213616240401";
@@ -22,6 +23,9 @@ class ForecastInfo {
     this.forecast = forecast;
   }
 }
+
+//Holds the information about the forecast on the current location
+let forecastInfo = new ForecastInfo();
 
 async function getUserLocation() {
   return new Promise((resolve, reject) => {
@@ -49,17 +53,97 @@ async function getUserLocation() {
 }
 
 async function retrieveForecastInfo(value) {
-  let result = await fetch(
-    `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${value}&days=7`,
-    { mode: "cors" }
-  );
-  let data = await result.json();
-  console.log(data);
+  return new Promise((resolve, reject) => {
+    fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${value}&days=14`,
+      { mode: "cors" }
+    )
+      .then((result) => {
+        return result.json();
+      })
+      .then((result) => {
+        resolve(result);
+      });
+  });
 }
 
-//Holds the information about the forecast on the current location
-let forecastInfo = new ForecastInfo();
+function setLocationData(locationData) {
+  let location = {};
+  location.name = locationData.name;
+  location.country = locationData.country;
+  forecastInfo.setLocation(location);
+}
+
+function setCurrentData(currentData, forecastData) {
+  let current = {};
+  current.condition = currentData.condition.text;
+  current.condition_code = currentData.condition.code;
+  current.temp_c = currentData.temp_c;
+  current.feelslike_c = currentData.feelslike_c;
+  current.humidity = currentData.humidity;
+  current.precip_mm = currentData.precip_mm;
+  current.pressure_mb = currentData.pressure_mb;
+  current.uv = currentData.uv;
+  current.wind_kph = currentData.wind_kph;
+  current.sunrise = forecastData.forecastday[0].astro.sunrise;
+  current.sunset = forecastData.forecastday[0].astro.sunset;
+  current.maxtemp_c = forecastData.forecastday[0].day.maxtemp_c;
+  current.mintemp_c = forecastData.forecastday[0].day.mintemp_c;
+  current.hourly = [];
+  forecastData.forecastday[0].hour.forEach((element) => {
+    let hourInfo = {};
+    hourInfo.time = element.time.split(" ")[1];
+    hourInfo.precip_mm = element.precip_mm;
+    hourInfo.condition = element.condition.text;
+    hourInfo.condition_code = element.condition.code;
+    hourInfo.temp_c = element.temp_c;
+    current.hourly.push(hourInfo);
+    hourInfo = [];
+  });
+  forecastInfo.setCurrent(current);
+}
+
+function setDailyData(forecastData) {
+  const WEEK_DAYS = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let forecast = [];
+  let dayInfo = {};
+  for (let i = 1; i < forecastData.forecastday.length; i++) {
+    dayInfo.date = forecastData.forecastday[i].date;
+    dayInfo.day = WEEK_DAYS[getDay(dayInfo.date)]; //getDay returns a number from 0 to 6 where 0 is Sunday
+    dayInfo.condition = forecastData.forecastday[i].day.condition.text;
+    dayInfo.condition_code = forecastData.forecastday[i].day.condition.code;
+    dayInfo.totalprecip_mm = forecastData.forecastday[i].day.totalprecip_mm;
+    dayInfo.avghumidity = forecastData.forecastday[i].day.avghumidity;
+    dayInfo.mintemp_c = forecastData.forecastday[i].day.mintemp_c;
+    dayInfo.maxtemp_c = forecastData.forecastday[i].day.maxtemp_c;
+    forecast.push(dayInfo);
+    dayInfo = {};
+  }
+  forecastInfo.setForecast(forecast);
+}
 
 getUserLocation().then((result) => {
-  retrieveForecastInfo(result);
+  retrieveForecastInfo(result).then((data) => {
+    console.log(data);
+    setLocationData(data.location);
+    setCurrentData(data.current, data.forecast);
+    setDailyData(data.forecast);
+    displayInfo(forecastInfo);
+  });
 });
+
+function displayInfo(forecastInfo) {
+  console.log(
+    forecastInfo.location,
+    forecastInfo.current,
+    forecastInfo.forecast
+  );
+}
